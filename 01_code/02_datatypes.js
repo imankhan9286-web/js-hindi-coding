13281061953672fd95d9f7282f0a6c4a763812b9
imankhan9286-web <imankhan9286@gmail.com>
2025-12-14 19:46:13 +0000

    conversionOperation

---
+++ 01_code/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3); // we are using nodejs, not brower
+
+console . log (3+3)
+console.log ("hello")
+
+
+let name = "halima"
+let age = 19 ;
+let isLoggedID = false
+
+
+// number => 2 to power  53 
+// bigint 
+// string => ""
+// bollean => true/false 
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console . log (typeof null); // object 
+console . log (typeof undefined); // undefined 
+
---
+++ 01_code/03_conversionOperation.js
@@ -0,0 +1,68 @@
+ let score = null                                  
+
+
+ //console. log (typeof score);
+  //console. log (typeof (score));
+
+
+  let valueInNumber = Number (score)
+ // console. log (typeof valueInNumber);
+ // console. log ( valueInNumber);
+
+ // "33"=> 33
+ // "33abc" =>NaN 
+ // true => 1; false =>0
+
+ let isLoggedID = " halima"
+
+ let booleanIsloggedIn = Boolean(isLoggedID)
+ //console . log (booleanIsloggedIn);
+
+ // 1 =>true; 0 => flase
+ // "" => flase ; 
+ // " halima " => true
+
+ //let someNumber = 33
+
+ //let stringNumber = String(someNumber)
+ //console.log (stringNumber);
+
+
+
+  //**************************** Operations ****************
+
+
+ let value = 3
+ let negValue = -value
+ //console . log (negValue);
+
+//console.log (2+2);
+//console.log (2-2);
+//console.log (2*2);
+//console.log (2%3);
+//console.log (2**3);
+//console.log (2/3); 
+
+//let str1 = " hello "
+ //let str2 = " halima "
+
+ //let str3 = str1 + str2
+ //console.log(str3);
+
+//onsole.log("1" +2);
+//console.log("1" + 2 +2);
+//onsole.log(1 +"2");
+//console.log(1+ 2 +"2");
+
+//console.log ((3+ 4) * 5 % 3);
+
+//console.log (+true);
+//console.log (+" ");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter ++;
+console.log(gameCounter);
