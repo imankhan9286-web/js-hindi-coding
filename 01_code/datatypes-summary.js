304cd2f8ccf0effe79d08d73aea4957063cc8757
imankhan9286-web <imankhan9286@gmail.com>
2026-01-01 18:01:13 +0000

    datatypes-summary

---
+++ 01_code/datatypes-summary.js
@@ -0,0 +1,25 @@
+ // Primitive Data Types
+ // 7 types: 
+ // String, Number, Boolean, null, undefined, Symbol, BigInt
+
+ const isLoggedIn = false;
+    const outsideTemp = null;
+    const user = undefined;
+    const id = Symbol('123');
+    const bigNumber = 123456789012345678901234567890n;
+
+ // Reference Data Types (non-primitive)
+
+ // Array, Object, Function
+
+
+    const colors = ['red', 'green', 'blue'];
+    let myobj ={
+        name: "halima",
+        age : 19
+    }
+
+    const myFunction = function() {
+        console.log("Hello world");
+    }
+     console.log(typeof anotherId ); 
