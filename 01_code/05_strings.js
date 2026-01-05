eea91cc4cf7c04c2302fd6f1d07c2a01a595600e
imankhan9286-web <imankhan9286@gmail.com>
2026-01-05 18:22:12 +0000

    05_string

--- 01_code/05_strings.js
+++ 01_code/05_strings.js
@@ -5,4 +5,34 @@
 
  console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
- const gameName = new string('halimahc')
+ const gameName = new String('halimahc')
+
+ //console.log(gameName [0]);
+// console.log(gameName.__proto__);
+
+
+
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase ());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('l'));
+
+ const newString = gameName.substring(0,6)
+ console.log(newString)
+
+ const anotherString = gameName.slice(-8,4);
+ console.log(anotherString);
+
+ const newStringOne = "   halima   "
+ console.log(newStringOne);
+ console.log(newStringOne. trim());
+
+ const url = "https://halima.com/halima%20khan"
+
+ console.log(url.replace('%20','-'))
+
+ console.log(url.includes('habiba'))
+
+ console.log(gameName.split(""));
+
+ 
