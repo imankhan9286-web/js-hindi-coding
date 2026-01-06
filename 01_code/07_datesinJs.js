5292234903fcbb353c679945e1e2b097230b6deb
imankhan9286-web <imankhan9286@gmail.com>
2026-01-06 16:13:12 +0000

    07 datesinJS

---
+++ 01_code/07_datesinJs.js
@@ -0,0 +1,11 @@
+ // Dates
+
+ let myDate = new Date()
+ //console.log(myDate.toString());
+ //console.log(myDate. toISOString());
+ //console.log(myDate. toDateString());
+ // console.log(myDate. toTimeString());
+ //console.log(myDate.toLocaleDateString())
+
+ //console.log(typeof myDate);
+ 
