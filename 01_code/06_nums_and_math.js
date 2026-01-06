f97b229feda4b676b63a4d3389b391f364b6df16
imankhan9286-web <imankhan9286@gmail.com>
2026-01-06 08:18:40 +0000

    06num and math

---
+++ 01_code/06_nums_and_math.js
@@ -0,0 +1,10 @@
+ const score = 400
+ console.log(score);
+
+ const balance = new number (100)
+ console.log(balance);
+
+ console.log(balance.toString ().length);
+ console.log(balance.toString(1));
+
+ const other
